973fe867a98df798217f7a089c79ef21d4af84b1
Victor-Estrella <victorhcarracci@gmail.com>
2025-11-21 15:41:17 -0300

    Atualiza a interface UsuarioApi para melhorar a documentação das operações relacionadas a usuários

--- src/main/java/br/com/fiap/SoulBalance/controller/UsuarioApi.java
+++ src/main/java/br/com/fiap/SoulBalance/controller/UsuarioApi.java
@@ -1,64 +1,55 @@
-package br.com.fiap.cp1.controller;
+package br.com.fiap.SoulBalance.controller;
 
-import br.com.fiap.cp1.dto.ProductRequest;
-import br.com.fiap.cp1.dto.ProductResponse;
+import br.com.fiap.SoulBalance.dto.UsuarioRequestDto;
+import br.com.fiap.SoulBalance.dto.UsuarioResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequestMapping("/products")
-@Tag(name = "Products", description = "Operações relacionadas ao cadastro de produtos")
-public interface ProductApi {
+@RequestMapping("/usuarios")
+@Tag(name = "Usuários", description = "Operações relacionadas a usuários")
+public interface UsuarioApi {
 
-    @Operation(summary = "Listar todos os produtos", description = "Retorna uma lista com todos os produtos cadastrados.")
+    @Operation(summary = "Listar todos os usuários", description = "Retorna uma lista de todos os usuários cadastrados.")
+    @ApiResponse(responseCode = "200", description = "Lista retornada com sucesso")
+    @GetMapping()
+    ResponseEntity<List<UsuarioResponseDto>> getAll();
+
+    @Operation(summary = "Buscar usuário por ID", description = "Retorna os dados de um usuário específico pelo ID.")
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "Lista de produtos retornada com sucesso")
+        @ApiResponse(responseCode = "200", description = "Usuário encontrado com sucesso"),
+        @ApiResponse(responseCode = "404", description = "Usuário não encontrado")
     })
-    @GetMapping
-    ResponseEntity<List<ProductResponse>> getAll();
+    @GetMapping("/{idUsuario}")
+    ResponseEntity<UsuarioResponseDto> getById(@PathVariable Long idUsuario);
 
-    @Operation(summary = "Buscar produto por ID", description = "Retorna os dados de um produto específico com base no ID informado.")
+    @Operation(summary = "Cadastrar novo usuário", description = "Cria um novo usuário com os dados fornecidos.")
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "Produto encontrado com sucesso"),
-            @ApiResponse(responseCode = "404", description = "Produto não encontrado")
-    })
-    @GetMapping("/{idProduct}")
-
-    ResponseEntity<ProductResponse> getById(@PathVariable Long idProduct);
-
-
-    @Operation(summary = "Cadastrar novo produto", description = "Cria um novo produto com os dados fornecidos.")
-    @ApiResponses({
-            @ApiResponse(responseCode = "201", description = "Produto criado com sucesso"),
-            @ApiResponse(responseCode = "400", description = "Dados inválidos para criação do produto")
+        @ApiResponse(responseCode = "201", description = "Usuário criado com sucesso"),
+        @ApiResponse(responseCode = "400", description = "Dados inválidos para criação do usuário")
     })
     @PostMapping
-    ResponseEntity<ProductResponse> create(@Valid @RequestBody ProductRequest request);
+    ResponseEntity<UsuarioResponseDto> save(@RequestBody UsuarioRequestDto filter) throws Exception;
 
-
-    @Operation(summary = "Atualizar dados do produto", description = "Atualiza as informações de um produto existente com base no ID.")
+    @Operation(summary = "Atualizar usuário", description = "Atualiza as informações de um usuário existente pelo ID.")
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "Produto atualizado com sucesso"),
-            @ApiResponse(responseCode = "400", description = "Dados inválidos para atualização"),
-            @ApiResponse(responseCode = "404", description = "Produto não encontrado")
+        @ApiResponse(responseCode = "200", description = "Usuário atualizado com sucesso"),
+        @ApiResponse(responseCode = "400", description = "Dados inválidos para atualização"),
+        @ApiResponse(responseCode = "404", description = "Usuário não encontrado")
     })
-    @PutMapping("/{idProduct}")
-    ResponseEntity<ProductResponse> update(
-            @PathVariable Long idProduct,
-            @Valid @RequestBody ProductRequest request);
+    @PutMapping("/{idUsuario}")
+    ResponseEntity<UsuarioResponseDto> update(@RequestBody UsuarioRequestDto filter, @PathVariable Long idUsuario);
 
-
-    @Operation(summary = "Excluir produto", description = "Remove um produto existente com base no ID fornecido.")
+    @Operation(summary = "Excluir usuário", description = "Remove um usuário existente pelo ID.")
     @ApiResponses({
-            @ApiResponse(responseCode = "204", description = "Produto excluído com sucesso"),
-            @ApiResponse(responseCode = "404", description = "Produto não encontrado")
+        @ApiResponse(responseCode = "204", description = "Usuário excluído com sucesso"),
+        @ApiResponse(responseCode = "404", description = "Usuário não encontrado")
     })
-    @DeleteMapping("/{idProduct}")
-    ResponseEntity<Void> delete(@PathVariable Long idProduct);
+    @DeleteMapping("/{idUsuario}")
+    ResponseEntity<Void> delete(@PathVariable Long idUsuario);
 }
